3adf50015ac0686758b92adba41378bb42aa920c
semyonix_pokemonix <sff1@tpu.ru>
2020-09-07 23:59:40 +0600

    update Observer

--- src/ts/Observer.pattern.ts
+++ src/ts/Observer.pattern.ts
@@ -6,58 +6,69 @@
   let addBtn:  JQuery<HTMLButtonElement> | null = $('.addNewObserver');
   let container:  JQuery<HTMLElement> | null = $('.observersContainer');
 
-  class Observer {
-    constructor() {}
-    // count() {
-    //   return this.observerList.length;
-    // }
+  interface ListCheckbox {
+    elem: JQuery<HTMLInputElement>,
+    value: boolean
   }
+  // interface setUpdate extends JQuery<HTMLElement> {
+  //   update?: typingFunction<string>
+  // }
   
-  class Subject extends Observer implements listSubject<setUpdate> {
-    public observerList: any[] = [];
-    constructor() {
-      super();
-    }
 
-    add(obj: setUpdate): void {
-      this.observerList.push(obj);
-    }
+  interface listSubject<T> {
+    // addObserver(key: string, fn: setUpdate): void,
+    // getObserver(index: number): HTMLElement | null,
+    // notify(context: HTMLElement): void
+  }
+  type typingFunction<C> = (value: C) => void;
 
-    get(index: number): HTMLElement | null {
-      if ( index > -1 && index < this.observerList.length ) {
-        return this.observerList[index];
-      }
-      return null;
+  type EventMap = Record<string, any>; // { data: string } => true, {x: string} => true
+  type EventKey<T extends EventMap> = string & keyof T;
+
+  class Subject<T extends EventMap> implements listSubject<T> {
+    public observerList: {
+      [K in keyof EventMap]?: Array<(p: EventMap[K]) => void>
+    } = {};
+    addObserver<K extends EventKey<T>>(key: K, fn: typingFunction<T[K]>): void {
+      this.observerList[key] = (this.observerList[key] || []).concat(fn);
     }
-    notify(context:HTMLElement) {
-      for(var i = 0, len = this.observerList.length; i < len; i++) {
-        let check = this.observerList[i];
-        check.update = context;
-      }
+    offObserver<K extends EventKey<T>>(key: K, fn: typingFunction<T[K]>): void {
+      this.observerList[key] = (this.observerList[key] || []).filter( (f: typingFunction<T[K]>) => f !== fn);
+    }
+    notify<K extends EventKey<T>>(key: K, data: T[K]): void {
+      (this.observerList[key] || []).forEach(function(fn: typingFunction<T[K]>) {
+        fn(data);
+      });
     }
   }
 
-  let subject = new Subject();
-  controlCheckbox.on('click', function(){
-    if ( controlCheckbox ) {
-      subject.notify( controlCheckbox.prop('checked') );
-    }
-  });
+  let subject = new Subject<{ setCheckbox: ListCheckbox }>();
+  subject.addObserver('setCheckbox', ({ elem, value }) => setCheckbox({ elem, value }));
+  
+  function setCheckbox(params: ListCheckbox): void {
+    params.elem.get(0).checked = params.value;
+  }
+  function updateCheckbox() {
+    $('.observer').each((i, element) => {
+      if ( controlCheckbox ) {
+        subject.notify('setCheckbox', { elem: $(element), value: controlCheckbox.prop('checked') } as ListCheckbox )
+      }
+    });
+  }
+
+  if ( controlCheckbox ) {
+    controlCheckbox.on('click', updateCheckbox);
+  }
   if ( addBtn ) {
     addBtn.on('click', addNewObserver);
   }
 
   function addNewObserver() {
-    var check = <setUpdate>$('<input type="checkbox">');
-    Object.defineProperty(check, 'update', {
-      set(value: string) {
-        this.get(0).checked = value;
-      }
-    });
-    subject.add(check);
+    var check = $('<input type="checkbox" class="observer">');
     if ( container ) {
       container.append(check);
-    }
+    } 
+    updateCheckbox();
   }
 
 })(jQuery);
--- src/ts/interface.ts
+++
@@ -1,7 +0,0 @@
-interface setUpdate extends JQuery<HTMLElement> {
-  update?: typingFunction<string>
-}
-interface listSubject<T> {
-  add(obj: T): void,
-}
-type typingFunction<C> = (value: C) => void;
